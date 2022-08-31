d8658dc4cede7bbfad2428d7dda131d689f93ed7
Simon C Blyth <simoncblyth@gmail.com>
2022-08-31 14:10:31 +0100

    switch to contains not prefix for value key matching

--- tests/NP_MakeValues_test.cc
+++ tests/NP_MakeValues_test.cc
@@ -1,4 +1,4 @@
-// name=NP_MakeValues_test ; mkdir -p /tmp/$name ;  gcc $name.cc -std=c++11 -lstdc++ -I.. -o /tmp/$name/$name && FOLD=/tmp/$name /tmp/$name/$name 
+// ./NP_MakeValues_test.sh 
 
 #include <cstdlib>
 #include "NP.hh"
@@ -10,12 +10,21 @@ int main(int argc, char** argv)
     std::vector<std::pair<std::string, double>> values ; 
     values.push_back( {"red", 1. } ); 
     values.push_back( {"green", 2. } ); 
+    values.push_back( {"green.pink", 200. } ); 
+    values.push_back( {"green.pink", 300. } ); 
+    values.push_back( {"green.pink", 400. } ); 
+    values.push_back( {"green", 2. } ); 
     values.push_back( {"blue", 3. } ); 
 
-    NP* vv = NP::MakeValues(values) ; 
+
+    NP* vv0 = NP::MakeValues(values) ; 
+    vv0->save(FOLD, "values0.npy" );    
+
+    NP* vv = NP::MakeValues(values, "pink") ; 
     vv->save(FOLD, "values.npy" );    
 
-    std::cout << vv->descValues() << std::endl; 
+    std::cout << "vv0" << std::endl << vv0->descValues() << std::endl; 
+    std::cout << "vv" << std::endl << vv->descValues() << std::endl; 
 
     return 0 ; 
 
