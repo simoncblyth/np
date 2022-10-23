10c82851b8faac000d78f16c1376d8f95ad5994e
Simon C Blyth <simoncblyth@gmail.com>
2022-10-23 20:14:07 +0100

    U::GetE

--- tests/UTest.cc
+++ tests/UTest.cc
@@ -3,16 +3,36 @@
 #include <string>
 #include "NPU.hh"
 
-int main(int argc, char** argv)
+void test_ChangeExt()
 {
- 
     const char* path = "/tmp/c.npy" ; 
     const char* xpath = "/tmp/c.npj" ; 
     std::string st = U::ChangeExt(path, ".npy", ".npj") ; 
-
     std::cout << st << std::endl ; 
-
     assert( strcmp(st.c_str(), xpath) == 0 ); 
+}
+
+void test_GetE()
+{
+    float  f   = U::GetE<float>("F", 101.5f );  
+    double d   = U::GetE<double>("D", 202.5  );  
+    int    i   = U::GetE<int>("I",  -303 );  
+    unsigned u = U::GetE<unsigned>("U", 404u  );  
+
+    std::cout 
+        << " F " << f << std::endl 
+        << " D " << d << std::endl 
+        << " I " << i << std::endl 
+        << " U " << u << std::endl 
+        ;
+}
+
+int main(int argc, char** argv)
+{
+    /*
+    test_ChangeExt(); 
+    */
+    test_GetE(); 
 
     return 0 ; 
 }
