8ee176a184e8397fd7e7b66206505f99015987bb
Simon C Blyth <simoncblyth@gmail.com>
2022-11-10 17:28:05 +0000

    make last column integer annotation survive NP::MakeNarrow when a metadata switch is flipped

---
+++ tests/NP_MakeNarrow_test.cc
@@ -0,0 +1,91 @@
+// ./NP_MakeNarrow_test.sh 
+
+#include "NP.hh"
+
+template <typename T>
+NP* MakeSrc(unsigned ni, unsigned nj)   // static 
+{
+    NP* a = NP::Make<T>( ni, nj );  
+    T* vv = a->values<T>(); 
+    for(unsigned i=0 ; i < ni ; i++)
+    {   
+        for( unsigned j=0 ; j < nj ; j++)
+        {   
+            switch(j)
+            {   
+                case 0:    vv[i*nj+j] = T(i)         ; break ; 
+                case 1:    vv[i*nj+j] = T(10*i)      ; break ; 
+                case 2:    vv[i*nj+j] = T(100*i)     ; break ; 
+                default:   vv[i*nj+j] = T(1000*i)    ; break ; 
+            }   
+        }   
+    }   
+    a->dump(); 
+    return a ; 
+}
+
+
+template<typename T>
+struct NP_MakeNarrow_test
+{
+    static const char* FOLD ; 
+
+    NP* a ; 
+    NP* b ; 
+    NP* c ; 
+    NP* abc ; 
+    NP* fabc ; 
+
+    NP_MakeNarrow_test(); 
+    void save() const ; 
+};
+
+template<typename T>
+const char* NP_MakeNarrow_test<T>::FOLD = U::GetEnv("FOLD", "/tmp") ; 
+
+template<typename T>
+NP_MakeNarrow_test<T>::NP_MakeNarrow_test()
+    :
+    a(MakeSrc<T>(10,2 )),
+    b(MakeSrc<T>( 2,2 )),
+    c(MakeSrc<T>( 5,2 )),
+    abc(NP::Combine(a,b,c)),
+    fabc(NP::MakeWithType<float>(abc))
+{
+    std::cout << " abc.sstr " << abc->sstr() << std::endl ;  
+    abc->dump(); 
+}
+ 
+template<typename T>
+void NP_MakeNarrow_test<T>::save() const 
+{
+    a->save(FOLD, "a.npy");  
+    b->save(FOLD, "b.npy");  
+    c->save(FOLD, "c.npy");  
+    abc->save(FOLD, "abc.npy");  
+    fabc->save(FOLD, "fabc.npy");  
+}
+
+int main(int argc, char** argv)
+{
+    NP_MakeNarrow_test<double> t ; 
+    t.save();  
+    return 0 ; 
+}
+
+/**
+Issue : Last column integer annotation gets scrubbed by narrowing (or widening presumably)::
+
+    In [2]: t.abc[...,-1,-1].view(np.int64)
+    Out[2]: array([10,  2,  5])
+
+    In [3]: t.fabc[...,-1,-1].view(np.int32)
+    Out[3]: array([0, 0, 0], dtype=int32)
+
+After fix the annotation survives being narrowed::
+
+    In [1]: t.fabc[...,-1,-1].view(np.int32)
+    Out[1]: array([10,  2,  5], dtype=int32)
+
+**/
+
---
+++ tests/NP_MakeNarrow_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l 
+
+name=NP_MakeNarrow_test 
+
+fold=/tmp/$name
+bin=$fold/$name
+mkdir -p $fold 
+
+export FOLD=$fold
+
+defarg="build_run_ana" 
+arg=${1:-$defarg}
+
+if [ "${arg/build}" != "$arg" ]; then 
+    #opt="-DDEBUG"
+    opt=""
+    gcc $name.cc -g $opt -std=c++11 -lstdc++ -I.. -o $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE build fail && exit 1 
+fi 
+
+if [ "${arg/run}" != "$arg" ]; then 
+    $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE run fail && exit 2
+fi 
+
+if [ "${arg/dbg}" != "$arg" ]; then 
+    case $(uname) in 
+       Darwin) lldb__ $bin ;;
+       Linux)  gdb__  $bin ;;
+    esac
+    [ $? -ne 0 ] && echo $BASH_SOURCE dbg fail && exit 3
+fi 
+
+if [ "${arg/ana}" != "$arg" ]; then 
+    ${IPYTHON:-ipython} --pdb -i $name.py 
+    [ $? -ne 0 ] && echo $BASH_SOURCE ana fail && exit 4
+fi 
+
+exit 0 
+
+
+
