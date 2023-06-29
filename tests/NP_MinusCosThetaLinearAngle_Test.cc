6273672bf82f0d40d41fc031559b0b8f3d9b94db
Simon C Blyth <simoncblyth@gmail.com>
2023-06-29 10:06:52 +0100

    NP::MinusCosThetaLinearAngle special case nx=1, add omitted inlines

--- tests/NP_MinusCosThetaLinearAngle_Test.cc
+++ tests/NP_MinusCosThetaLinearAngle_Test.cc
@@ -1,11 +1,24 @@
-// name=NP_MinusCosThetaLinearAngle_Test ; gcc $name.cc -std=c++11 -lstdc++ -I.. -o /tmp/$name && /tmp/$name
+// ./NP_MinusCosThetaLinearAngle_Test.sh
 
 #include "NP.hh"
 
 int main(int argc, char** argv)
 {
-    NP* a = NP::MinusCosThetaLinearAngle<double>(181); 
-    a->save("/tmp/a.npy"); 
+    NP* mct1 = NP::MinusCosThetaLinearAngle<double>(1); 
+    NP* mct181 = NP::MinusCosThetaLinearAngle<double>(181); 
+    NP* mct900 = NP::MinusCosThetaLinearAngle<double>(900); 
+
+    NP* st1 = NP::SqrtOneMinusSquare(mct1) ; 
+    NP* st181 = NP::SqrtOneMinusSquare(mct181) ; 
+    NP* st900 = NP::SqrtOneMinusSquare(mct900) ; 
+
+    mct1->save("$FOLD/mct1.npy"); 
+    mct181->save("$FOLD/mct181.npy"); 
+    mct900->save("$FOLD/mct900.npy"); 
+
+    st1->save("$FOLD/st1.npy"); 
+    st181->save("$FOLD/st181.npy"); 
+    st900->save("$FOLD/st900.npy"); 
 
     return 0 ; 
 }
