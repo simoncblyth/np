b37fa709db3fafb3429421e3d2c5c3ae692c7bb6
Simon C Blyth <simoncblyth@gmail.com>
2023-07-07 17:31:01 +0100

    NPFold::find_subfold_with_all_keys eg for finding materials with scintillation props

---
+++ tests/NPFold_has_all_keys_test.cc
@@ -0,0 +1,48 @@
+// name=NPFold_has_all_keys_test ; gcc $name.cc -std=c++11 -lstdc++ -I.. -o /tmp/$name && /tmp/$name
+
+#include "NPFold.h"
+
+int main(int argc, char** argv)
+{
+    NPFold* f0 = new NPFold ; 
+    f0->add("red", NP::Linspace<int>(0, 100, 101) ); 
+    f0->add("green", NP::Linspace<double>(0, 100, 101) ); 
+    f0->add("blue", NP::Linspace<double>(0, 100, 101) ); 
+
+    assert( f0->has_all_keys("red") == true ); 
+    assert( f0->has_all_keys("red,green") == true ); 
+    assert( f0->has_all_keys("red,green,blue") == true ); 
+    assert( f0->has_all_keys("red,green,blue,cyan") == false ); 
+
+    NPFold* f1 = new NPFold ; 
+    f1->add("red", NP::Linspace<int>(0, 100, 101) ); 
+    f1->add("green", NP::Linspace<double>(0, 100, 101) ); 
+     
+    NPFold* f2 = new NPFold ; 
+    f2->add("red", NP::Linspace<int>(0, 100, 101) ); 
+   
+
+    NPFold* ff = new NPFold ; 
+    ff->add_subfold( "f0", f0 ); 
+    ff->add_subfold( "f1", f1 ); 
+    ff->add_subfold( "f2", f2 ); 
+
+
+    std::vector<const NPFold*> subs0 ; 
+    ff->find_subfold_with_all_keys( subs0, "red" ); 
+    assert( subs0.size() == 3 ); 
+
+
+    std::vector<const NPFold*> subs1 ; 
+    ff->find_subfold_with_all_keys( subs1, "red,green" ); 
+    assert( subs1.size() == 2 ); 
+
+    std::vector<const NPFold*> subs2 ; 
+    ff->find_subfold_with_all_keys( subs2, "red,green,blue" ); 
+    assert( subs2.size() == 1 ); 
+
+    return 0 ; 
+}
+
+
+
