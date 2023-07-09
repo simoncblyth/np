5d37d48689b094e28ee1580fb74ecec5081370b0
Simon C Blyth <simoncblyth@gmail.com>
2023-07-09 16:05:16 +0100

    special case handling of empty arrays with .txt keys, treating as NPX::Holder arrays that simply carry vectors of strings via names metadata

--- tests/NPX_Holder_test.cc
+++ tests/NPX_Holder_test.cc
@@ -1,12 +1,24 @@
-// name=NPX_Holder_test ; gcc $name.cc -std=c++11 -lstdc++ -I.. -o /tmp/$name && /tmp/$name
+// ./NPX_Holder_test.sh 
 
+#include "NPFold.h"
 #include "NPX.h"
 
 int main()
 {
     std::vector<std::string> nn = {"red","green", "blue" } ; 
     NP* a = NPX::Holder(nn); 
-    a->save("/tmp/a.npy") ; 
+
+    NP* b = NP::Make<int>(1) ; 
+
+    std::cout << " a " << a->sstr() << std::endl ; 
+
+    std::cout << " a.is_empty " << a->is_empty() << std::endl ; 
+    std::cout << " b.is_empty " << b->is_empty() << std::endl ; 
+
+    NPFold* fold = new NPFold ; 
+    fold->add("a.txt", a ); 
+    fold->add("b", b ); 
+    fold->save("$FOLD"); 
 
     return 0 ; 
 }
