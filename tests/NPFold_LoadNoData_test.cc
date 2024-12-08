7e0495775899d46b31130de68ef48d7e56b6e04c
Simon C Blyth <simoncblyth@gmail.com>
2024-12-08 16:09:25 +0800

    add maxdepth control to avoid NPFold::find_subfold_with_prefix needed for opticks/sysrap/tests/sreport.cc when multi-launch subfold are kept

--- tests/NPFold_LoadNoData_test.cc
+++ tests/NPFold_LoadNoData_test.cc
@@ -1,16 +1,75 @@
-// ~/np/tests/NPFold_LoadNoData_test.sh
+/**
+
+~/np/tests/NPFold_LoadNoData_test.sh
+TEST=run ~/np/tests/NPFold_LoadNoData_test.sh
+TEST=substamp ~/np/tests/NPFold_LoadNoData_test.sh
+
+
+**/
 
 #include "NPFold.h"
 
-int main(int argc, char** argv)
+struct NPFold_LoadNoData_test
+{
+    static const char* TEST ; 
+    static bool VERBOSE ;
+    static int main(int argc, char** argv);
+
+    NPFold* f ; 
+    NPFold_LoadNoData_test(const char* dirp); 
+
+    int run();  
+    int substamp();  
+
+    int test(); 
+}; 
+
+const char* NPFold_LoadNoData_test::TEST = U::GetEnv("TEST", "ALL") ; 
+bool        NPFold_LoadNoData_test::VERBOSE = getenv("VERBOSE") != nullptr ; 
+
+int NPFold_LoadNoData_test::main(int argc, char** argv)
 {
     const char* dirp = argc > 1 ? argv[1] : U::PWD() ;   
     if(dirp == nullptr) return 0 ; 
 
-    bool VERBOSE = getenv("VERBOSE") != nullptr ; 
- 
-    NPFold* f = NPFold::LoadNoData(dirp); 
+    std::cout 
+        << argv[0]
+        << std::endl
+        << "NPFold::LoadNoData(\"" << dirp << "\")" 
+        << std::endl
+        ;   
 
+
+
+    NPFold_LoadNoData_test t(dirp); 
+    return t.test(); 
+}
+
+
+NPFold_LoadNoData_test::NPFold_LoadNoData_test(const char* dirp)
+    :
+    f(NPFold::LoadNoData(dirp))
+{
+    if(VERBOSE) std::cout 
+        << "[" << ".VERBOSE\n"
+        << ( f ? f->desc() : "-" ) << "\n"
+        << "]" << ".VERBOSE\n"
+        ;
+}
+
+
+int NPFold_LoadNoData_test::test()
+{
+    bool ALL = strcmp(TEST, "ALL") == 0 ; 
+    int rc = 0 ; 
+    if(ALL||0==strcmp(TEST, "run"))      rc += run(); 
+    if(ALL||0==strcmp(TEST, "substamp")) rc += substamp(); 
+    return rc ; 
+}
+
+
+int NPFold_LoadNoData_test::run()
+{
     const NP* run = f->get("run"); 
     std::cout 
         << " run " << ( run ? run->sstr() : "-" ) 
@@ -22,26 +81,23 @@ int main(int argc, char** argv)
         << ( run ? run->descMetaKVS() : "-" ) 
         << std::endl 
         ; 
-
-
-    std::cout 
-        << argv[0]
-        << std::endl
-        << "NPFold::LoadNoData(\"" << dirp << "\")" 
-        << std::endl
-        ;   
-
-    if(VERBOSE) std::cout 
-        << "[" << argv[0] << ".VERBOSE "
-        << std::endl
-        << ( f ? f->desc() : "-" )
-        << std::endl
-        << "]" << argv[0] << ".VERBOSE "
-        << std::endl
-        ;
-
     return 0 ; 
 }
+
+int NPFold_LoadNoData_test::substamp()
+{
+    NPFold* s = f->subfold_summary("substamp", "a://A", "b://B"); 
+    assert(s); 
+    return 0 ; 
+}
+
+
+
+int main(int argc, char** argv)
+{
+    return NPFold_LoadNoData_test::main(argc, argv); 
+}
+
 
 // ~/np/tests/NPFold_LoadNoData_test.sh
 
--- tests/NPFold_LoadNoData_test.sh
+++ tests/NPFold_LoadNoData_test.sh
@@ -7,25 +7,58 @@ NPFold_LoadNoData_test.sh
 
    ~/np/tests/NPFold_LoadNoData_test.sh
 
+   TEST=substamp ~/np/tests/NPFold_LoadNoData_test.sh dbg
+
 
 EOU
 }
+cd $(dirname $(realpath $BASH_SOURCE))
+source dbg__.sh
+SDIR=$(pwd)
 
-SDIR=$(cd $(dirname $BASH_SOURCE) && pwd)
 name=NPFold_LoadNoData_test 
 TMP=${TMP:-/tmp/$USER/opticks}
 bin=$TMP/$name
 
+#jdir=/data/blyth/opticks/GEOM/J23_1_0_rc3_ok0/jok-tds/ALL0
+jdir=/data/blyth/opticks/GEOM/J_2024nov27/CSGOptiXSMTest/ALL1
+JDIR=${JDIR:-$jdir}
+
+test=substamp
+TEST=${TEST:-$test}
+
+defarg=info_build_run
+arg=${1:-$defarg}
 
 
-gcc $SDIR/$name.cc -std=c++11 -lstdc++ -I$SDIR/.. -o $bin 
-[ $? -ne 0 ] && echo $BASH_SOURCE : build error && exit 1
+export NPFold__substamp_DUMP=1
 
-cd /data/blyth/opticks/GEOM/J23_1_0_rc3_ok0/jok-tds/ALL0
-[ $? -ne 0 ] && echo $BASH_SOURCE : no such directory && exit 0
+vars="BASH_SOURCE name SDIR PWD bin defarg arg JDIR test TEST"
 
-$bin
-[ $? -ne 0 ] && echo $BASH_SOURCE : run error && exit 3
+if [ "${arg/info}" != "$arg" ]; then
+   for var in $vars ; do printf "%20s : %s\n" "$var" "${!var}" ; done 
+fi 
+
+if [ "${arg/build}" != "$arg" ]; then
+   gcc $SDIR/$name.cc -g -std=c++11 -lstdc++ -I$SDIR/.. -o $bin 
+   [ $? -ne 0 ] && echo $BASH_SOURCE : build error && exit 1
+fi 
+
+if [ "${arg/run}" != "$arg" ]; then
+    cd $JDIR
+    [ $? -ne 0 ] && echo $BASH_SOURCE : no such directory && exit 0
+    $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE : run error && exit 3
+fi
+
+
+if [ "${arg/dbg}" != "$arg" ]; then
+    cd $JDIR
+    [ $? -ne 0 ] && echo $BASH_SOURCE : no such directory && exit 0
+    dbg__ $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE : dbg error && exit 4
+fi
+
 
 exit 0 
 
