a7c7228c31488851027a563ddf5b7ccac30e585d
Simon C Blyth <simoncblyth@gmail.com>
2024-12-17 17:15:51 +0800

    generalizing metadata time stamp handling for duplicated keys as happens with multi-launch running

--- tests/NP_DescMetaKVS_ranges_test.cc
+++ tests/NP_DescMetaKVS_ranges_test.cc
@@ -2,50 +2,41 @@
 
 #include "NP.hh"
 
-int main()
+struct NP_DescMetaKVS_ranges_test
 {
-    std::vector<std::string> keys = { 
-         "SEvt__Init_RUN_META", 
-         "CSGFoundry__Load_HEAD",
-         "CSGFoundry__Load_TAIL",
-         "CSGOptiX__Create_HEAD",
-         "CSGOptiX__Create_TAIL", 
-         "A000_QSim__simulate_HEAD",
-         "A000_QSim__simulate_PREL",
-         "A000_QSim__simulate_POST",
-         "A000_QSim__simulate_TAIL",
-         "A001_QSim__simulate_HEAD",
-         "A001_QSim__simulate_PREL",
-         "A001_QSim__simulate_POST",
-         "A001_QSim__simulate_TAIL",
-         "A002_QSim__simulate_HEAD",
-         "A002_QSim__simulate_PREL",
-         "A002_QSim__simulate_POST",
-         "A002_QSim__simulate_TAIL",
-         "CSGOptiX__SimulateMain_TAIL"
-      }; 
+    static int ranges(); 
+    static int ranges2(); 
+    static int Main(); 
+}; 
 
-    std::vector<int64_t> tt = {
-         1, 
-         2,
-         3,
-         4,
-         5,
-         101,
-         102,
-         103,
-         104,
-         201,
-         202,
-         203,
-         204,
-         301,
-         302,
-         303,
-         304,
-         400
-     };
 
+int NP_DescMetaKVS_ranges_test::ranges()
+{
+    std::cout << "[NP_DescMetaKVS_ranges_test::ranges\n" ; 
+    std::vector<std::tuple<std::string,int64_t>> kt = {
+       {  "SEvt__Init_RUN_META", 1  },
+       {  "CSGFoundry__Load_HEAD", 2 },
+       {  "CSGFoundry__Load_TAIL", 3 },
+       {  "CSGOptiX__Create_HEAD", 4 },
+       {  "CSGOptiX__Create_TAIL", 5 }, 
+       {  "A000_QSim__simulate_HEAD", 101 },
+       {  "A000_QSim__simulate_PREL", 102 },
+       {  "A000_QSim__simulate_POST", 103 },
+       {  "A000_QSim__simulate_TAIL", 104 },
+       {  "A001_QSim__simulate_HEAD", 201 },
+       {  "A001_QSim__simulate_PREL", 202 },
+       {  "A001_QSim__simulate_POST", 203 },
+       {  "A001_QSim__simulate_TAIL", 204 },  
+       {  "A002_QSim__simulate_HEAD", 301 },
+       {  "A002_QSim__simulate_PREL", 302 },
+       {  "A002_QSim__simulate_POST", 303 },
+       {  "A002_QSim__simulate_TAIL", 304 },
+       {  "CSGOptiX__SimulateMain_TAIL", 400 }
+        } ; 
+ 
+    std::vector<std::string> keys ; 
+    std::vector<int64_t> tt ; 
+    U::SplitTuple( keys, tt, kt ); 
     assert( keys.size() == tt.size() ) ; 
 
     static constexpr const char* RANGES = R"( 
@@ -60,17 +51,79 @@ int main()
 
 
 
-
     std::cout << NP::DescMetaKVS_ranges(keys, tt, RANGES ) << std::endl ;
 
-
     NP* rr = NP::MakeMetaKVS_ranges(keys, tt, RANGES ) ; 
+    std::cout << "rr\n" << rr->descTable<int64_t>(20) ; 
 
-    std::cout << rr->descTable<int64_t>(20) ; 
-
-
+    std::cout << "]NP_DescMetaKVS_ranges_test::ranges\n" ; 
     return 0 ; 
 }
+
+
+int NP_DescMetaKVS_ranges_test::ranges2()
+{
+    std::cout << "[NP_DescMetaKVS_ranges_test::ranges2\n" ; 
+
+    std::vector<std::tuple<std::string,int64_t>> kt = {
+       {  "SEvt__Init_RUN_META", 10  },
+       {  "CSGFoundry__Load_HEAD", 20 },
+       {  "CSGFoundry__Load_TAIL", 30 },
+       {  "CSGOptiX__Create_HEAD", 40 },
+       {  "CSGOptiX__Create_TAIL", 50 }, 
+       {  "A000_QSim__simulate_HEAD", 1010 },
+       {  "A000_QSim__simulate_PREL", 1020 },
+       {  "A000_QSim__simulate_POST", 1030 },
+       {  "A000_QSim__simulate_PREL", 1031 },
+       {  "A000_QSim__simulate_POST", 1038 },
+       {  "A000_QSim__simulate_TAIL", 1040 },
+       {  "A001_QSim__simulate_HEAD", 2010 },
+       {  "A001_QSim__simulate_PREL", 2020 },
+       {  "A001_QSim__simulate_POST", 2030 },
+       {  "A001_QSim__simulate_TAIL", 2040 },  
+       {  "A002_QSim__simulate_HEAD", 3010 },
+       {  "A002_QSim__simulate_PREL", 3020 },
+       {  "A002_QSim__simulate_POST", 3030 },
+       {  "A002_QSim__simulate_TAIL", 3040 },
+       {  "CSGOptiX__SimulateMain_TAIL", 4000 }
+        } ; 
+ 
+    std::vector<std::string> keys ; 
+    std::vector<int64_t> tt ; 
+    U::SplitTuple( keys, tt, kt ); 
+    assert( keys.size() == tt.size() ) ; 
+
+    static constexpr const char* RANGES = R"( 
+        SEvt__Init_RUN_META:CSGFoundry__Load_HEAD       
+        CSGFoundry__Load_HEAD:CSGFoundry__Load_TAIL     ## load geom 
+        CSGOptiX__Create_HEAD:CSGOptiX__Create_TAIL     ## upload geom
+        A%0.3d_QSim__simulate_HEAD:A%0.3d_QSim__simulate_PREL         ## upload genstep
+        A%0.3d_QSim__simulate_PREL:A%0.3d_QSim__simulate_POST         ## simulate kernel
+        A%0.3d_QSim__simulate_POST:A%0.3d_QSim__simulate_TAIL         ## download 
+        A%0.3d_QSim__simulate_TAIL:CSGOptiX__SimulateMain_TAIL
+       )" ; 
+
+
+    std::cout << NP::DescMetaKVS_ranges2(keys, tt, RANGES ) << std::endl ;
+
+    NP* rr = NP::MakeMetaKVS_ranges2(keys, tt, RANGES ) ; 
+    std::cout << "rr\n" << rr->descTable<int64_t>(20) ; 
+
+    std::cout << "]NP_DescMetaKVS_ranges_test::ranges2\n" ; 
+    return 0 ; 
+}
+
+int NP_DescMetaKVS_ranges_test::Main()
+{
+    const char* TEST = U::GetEnv( "TEST", "ranges2" ); 
+    bool ALL = strcmp(TEST, "ALL") == 0 ; 
+    int rc = 0 ;
+    if(ALL||0==strcmp(TEST,"ranges"))  rc += ranges(); 
+    if(ALL||0==strcmp(TEST,"ranges2")) rc += ranges2(); 
+    return rc ; 
+}
+
+int main(int argc, char** argv){ return NP_DescMetaKVS_ranges_test::Main() ; }
 
 // ~/np/tests/NP_DescMetaKVS_ranges_test.sh
 
--- tests/NP_DescMetaKVS_ranges_test.sh
+++ tests/NP_DescMetaKVS_ranges_test.sh
@@ -9,11 +9,29 @@ EOU
 
 
 cd $(dirname $(realpath $BASH_SOURCE))
+source dbg__.sh 
 
 name=NP_DescMetaKVS_ranges_test
 bin=/tmp/$name
 
-gcc $name.cc -g -Wall -std=c++11 -lstdc++ -I.. -o $bin  && $bin 
+defarg="build_run"
+arg=${1:-$defarg}
 
+if [ "${arg/build}" != "$arg" ]; then
+   gcc $name.cc -g -Wall -std=c++11 -lstdc++ -I.. -o $bin  
+   [ $? -ne 0 ] && echo $BASH_SOURCE build error && exit 1
+fi 
+
+if [ "${arg/run}" != "$arg" ]; then
+   $bin 
+   [ $? -ne 0 ] && echo $BASH_SOURCE run error && exit 2
+fi
+
+if [ "${arg/dbg}" != "$arg" ]; then
+   dbg__ $bin 
+   [ $? -ne 0 ] && echo $BASH_SOURCE dbg error && exit 3
+fi
+
+exit 0 
 
 
