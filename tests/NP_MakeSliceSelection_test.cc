65611063da8da0ddb98635b5644dd07391a27fb2
Simon C Blyth <simoncblyth@gmail.com>
2025-05-26 14:18:15 +0800

    add NP::MakeSliceSelection handling subset of numpy-like selection strings such as [:,0,0,0] < 0.5

---
+++ tests/NP_MakeSliceSelection_test.cc
@@ -0,0 +1,102 @@
+// ./NP_MakeSliceSelection_test.sh
+
+#include "NP.hh"
+#include "NPFold.h"
+
+int test_ParseSliceString()
+{
+    std::vector<NP::INT> idxx ;
+    const char* sli = "  [:,0,0,2]<0.5  " ;
+    NP::ParseSliceString(idxx, sli );
+
+    std::cout << "test_ParseSliceString idxx.size " << idxx.size() << "\n" ;
+
+    assert( idxx.size() == 4 );
+    assert( idxx[0] == -1 );
+    assert( idxx[1] ==  0 );
+    assert( idxx[2] ==  0 );
+    assert( idxx[3] ==  2 );
+    return 0 ;
+}
+
+int test_makeWhereSelection()
+{
+    NP* src = NP::Load("$AFOLD/record.npy");
+    std::vector<std::string> sels = {{
+       "  [:,0,0,0] < 0  ",
+       "  [:,0,0,0] > 0  ",
+       "  [:,0,0,0] < 0.5  ",
+       "  [:,0,0,0] > 0.5  "
+    }} ;
+
+    std::cout << "[test_makeWhereSelection\n" ;
+    for(int i=0 ; i < int(sels.size()) ; i++)
+    {
+        const char* _sel = sels[i].c_str();
+        NP* sel = src->makeWhereSelection<float>( _sel );
+        std::cout
+            << " _sel " << std::setw(20) << _sel
+            << " sel " << std::setw(20) << ( sel ? sel->sstr() : "-" )
+            << "\n"
+            ;
+    }
+    std::cout << "]test_makeWhereSelection\n" ;
+    return 0 ;
+}
+
+
+int test_MakeSliceSelection()
+{
+    NP* src = NP::Load("$AFOLD/record.npy");
+
+    std::vector<std::string> sels = {{
+       "  [:,0,0,0] < 0  ",
+       "  [:,0,0,0] > 0  ",
+       "  [:,0,0,0] < 0.5  ",
+       "  [:,0,0,0] > 0.5  "
+    }} ;
+    std::vector<std::string> labs = {{
+       "a",
+       "b",
+       "c",
+       "d"
+    }};
+
+    NPFold* fold = new NPFold ;
+
+    std::cout << "[test_MakeSliceSelection src " << ( src ? src->sstr() : "-" ) << "\n" ;
+    for(int i=0 ; i < int(sels.size()) ; i++)
+    {
+        const char* _sel = sels[i].c_str();
+        const char* _lab = labs[i].c_str();
+        NP* dst = NP::MakeSliceSelection<float>( src,  _sel );
+        std::cout
+            << " _sel " << std::setw(20) << _sel
+            << " dst " << std::setw(20) << ( dst ? dst->sstr() : "-" )
+            << "\n"
+            ;
+       fold->add(_lab, dst);
+
+    }
+    std::cout << "]test_MakeSliceSelection\n" ;
+    fold->save("$FOLD/MakeSliceSelection");
+    return 0 ;
+}
+
+
+int main(int argc, char** argv)
+{
+    //const char* DEFTEST = "ParseSliceString" ;
+    //const char* DEFTEST = "makeWhereSelection" ;
+    const char* DEFTEST = "MakeSliceSelection" ;
+    const char* TEST = U::GetEnv("TEST", DEFTEST) ;
+    bool ALL = strcmp(TEST, "ALL") == 0 ;
+    int rc = 0 ;
+    if(ALL||0==strcmp(TEST,"ParseSliceString"))   rc += test_ParseSliceString();
+    if(ALL||0==strcmp(TEST,"makeWhereSelection")) rc += test_makeWhereSelection();
+    if(ALL||0==strcmp(TEST,"MakeSliceSelection")) rc += test_MakeSliceSelection();
+
+
+    return rc ;
+}
+
---
+++ tests/NP_MakeSliceSelection_test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+usage(){ cat << EOU
+
+~/np/tests/NP_MakeSliceSelection_test.sh
+
+EOU
+}
+
+source $HOME/.opticks/GEOM/GEOM.sh
+source $HOME/.opticks/GEOM/EVT.sh
+
+
+cd $(dirname $(realpath $BASH_SOURCE))
+export PYTHONPATH=../..
+source dbg__.sh
+
+deftest=NP_MakeSliceSelection_test
+name=${TEST:-$deftest}
+
+export FOLD=/tmp/$name
+bin=$FOLD/$name
+script=$name.py
+mkdir -p $FOLD
+
+other=
+export OTHER=$other
+
+#opt="-DDEBUG"
+opt=""
+
+defarg="info_build_run_ana"
+arg=${1:-$defarg}
+
+vv="GEOM AFOLD BFOLD FOLD"
+
+if [ "${arg/info}" != "$arg" ]; then
+    for v in $vv ; do printf "%20s : %s\n" "$v" "${!v}" ; done
+fi
+
+if [ "${arg/build}" != "$arg" ]; then
+    gcc $name.cc -g $opt -std=c++17 -lstdc++ -I.. -o $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE build fail && exit 1
+fi
+
+if [ "${arg/run}" != "$arg" ]; then
+    $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE run fail && exit 2
+fi
+
+if [ "${arg/dbg}" != "$arg" ]; then
+    dbg__  $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE dbg fail && exit 3
+fi
+
+if [ "${arg/pdb}" != "$arg" ]; then
+    ${IPYTHON:-ipython} --pdb -i $script
+    [ $? -ne 0 ] && echo $BASH_SOURCE pdb fail && exit 4
+fi
+
+if [ "${arg/ana}" != "$arg" ]; then
+    ${PYTHON:-python} $script
+    [ $? -ne 0 ] && echo $BASH_SOURCE ana fail && exit 4
+fi
+
+exit 0
+
+
