6453b5938d21841cd8462d23a35ac055c822dac1
Simon C Blyth <simoncblyth@gmail.com>
2025-06-15 19:38:24 +0800

    change behaviour of NPFold::save suppressing the writing of empty folders that just contain metadata unless set_allowonlymeta_r

--- tests/NPFold_save_load_empty_test.cc
+++ tests/NPFold_save_load_empty_test.cc
@@ -1,33 +1,52 @@
-// ~/np/tests/NPFold_save_load_empty_test.sh
+/**
+NPFold_save_load_empty_test.cc
+=================================
+
+::
+
+     ~/np/tests/NPFold_save_load_empty_test.sh
+
+
+
+
+
+**/
 
 #include "NPFold.h"
 
 
 int main(int argc, char** argv)
 {
-    NPFold* a = new NPFold ; 
-    a->add("a", NP::Make<int>(1) ); 
+    NPFold* a = new NPFold ;
+    a->add("a", NP::Make<int>(1) );
 
-    NPFold* b = new NPFold ; 
-    //b->add("b", NP::Make<int>(1) ); 
+    NPFold* b = new NPFold ;
 
-    NPFold* c = new NPFold ; 
-    c->add("c", NP::Make<int>(1) ); 
-
-
-    NPFold* fold = new NPFold ; 
-    fold->add_subfold( "a", a  ); 
-    fold->add_subfold( "b", b  ); 
-    fold->add_subfold( "c", c  ); 
-    fold->set_allowempty_r(); 
-
-    fold->save("$FOLD"); 
-
-    NPFold* fold1 = NPFold::Load("$FOLD") ; 
-    std::cout << " fold1 " << fold1->desc() << std::endl ; 
+    //b->add("b", NP::Make<int>(1) );
+    std::string bmeta = "fold_with_just_metadata_not_saved_unless_set_allowonly_meta" ;
+    b->set_meta<std::string>("bmeta", bmeta);
 
 
-    return 0 ; 
+    NPFold* c = new NPFold ;
+    c->add("c", NP::Make<int>(1) );
+
+
+    NPFold* fold = new NPFold ;
+    fold->add_subfold( "a", a  );
+    fold->add_subfold( "b", b  );
+    fold->add_subfold( "c", c  );
+
+    // THE DEFAULTS OF THE BELOW ARE BOTH FALSE
+    //fold->set_allowempty_r();
+    //fold->set_allowonlymeta_r();
+
+    fold->save("$FOLD");
+
+    NPFold* fold1 = NPFold::Load("$FOLD") ;
+    std::cout << " fold1 " << fold1->desc() << std::endl ;
+
+
+    return 0 ;
 }
 
 // ~/np/tests/NPFold_save_load_empty_test.sh
--- tests/NPFold_save_load_empty_test.sh
+++ tests/NPFold_save_load_empty_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 usage(){ cat << EOU
 NPFold_save_load_empty_test.sh
-==========================
+================================
 
 ::
 
@@ -12,19 +12,19 @@ EOU
 }
 
 cd $(dirname $(realpath $BASH_SOURCE))
-source dbg__.sh 
+source dbg__.sh
 
-name=NPFold_save_load_empty_test 
+name=NPFold_save_load_empty_test
 TMP=${TMP:-/tmp/$USER/opticks}
 bin=$TMP/$name
 
 export FOLD=$TMP/${name}_fold
-mkdir -p $FOLD 
+mkdir -p $FOLD
 rm -rf $FOLD
 
 vars="name SDIR FOLD"
 
-defarg="info_build_run"
+defarg="info_build_run_find"
 arg=${1:-$defarg}
 
 if [ "${arg/info}" != "$arg" ]; then
@@ -32,20 +32,34 @@ if [ "${arg/info}" != "$arg" ]; then
 fi
 
 if [ "${arg/build}" != "$arg" ]; then
-    gcc $name.cc -std=c++11 -Wall -g -lstdc++ -I.. -o $bin 
+    gcc $name.cc -std=c++17 -Wall -g -lstdc++ -I.. -o $bin
     [ $? -ne 0 ] && echo $BASH_SOURCE : build error && exit 1
-fi 
+fi
 
 
 if [ "${arg/run}" != "$arg" ]; then
    $bin
    [ $? -ne 0 ] && echo $BASH_SOURCE : run error && exit 2
-fi 
+fi
 
 if [ "${arg/dbg}" != "$arg" ]; then
    dbg__ $bin
    [ $? -ne 0 ] && echo $BASH_SOURCE : dbg error && exit 2
-fi 
+fi
 
-exit 0 
+if [ "${arg/find}" != "$arg" ]; then
+   cmd="find $FOLD -type f"
+   echo $cmd
+   eval $cmd
+
+   mpath=$FOLD/b/NPFold_meta.txt
+   if [ -f "$mpath" ]; then
+      cmd="cat $mpath"
+      echo $cmd
+      eval $cmd
+   fi
+fi
+
+
+exit 0
 
