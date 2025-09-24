9cf89a56ab7bb309b48e5c766144d83aad015004
Simon C Blyth <simoncblyth@gmail.com>
2025-09-24 22:41:11 +0800

    try sneaking the metadata into the serialization payload after the array data

--- tests/NP_ReadToBufferCallback_test.cc
+++ tests/NP_ReadToBufferCallback_test.cc
@@ -12,11 +12,19 @@ int main()
 {
     NP* a = NP::Make<float>(1,4,4);
     a->fillIndexFlat();
+    a->set_meta<int>("red", 1);
+    a->set_meta<int>("green", 2);
+    a->set_meta<int>("blue", 3);
+    a->set_meta<float>("cyan", 2.14);
+    a->set_meta<float>("pi", M_PI);
+
+    std::cout << "a.meta[\n" << a->meta << "]\n" ;
+
     a->save("$FOLD/a0.npy");
     //a->update_headers();  // saving does this already
 
     std::vector<char> buf0 ;
-    a->serializeToBuffer(buf0);
+    a->serializeToBuffer(buf0, 1, 1);
     std::cout
          << "[NP_ReadToBufferCallback_test.HexDump.buf0\n"
          << NP::HexDump( buf0.data(), buf0.size() )
@@ -35,15 +43,13 @@ int main()
     NP* a3 = nullptr ;
     {
         std::cout << "[NP_ReadToBufferCallback_test.CreateFromBuffer\n" ;
-        size_t size = 129 ;
+        size_t size = 1 ;
         size_t nitems = 1;
         a3 = NP::CreateFromBuffer( buf0, size, nitems);
         std::cout << "-NP_ReadToBufferCallback_test.CreateFromBuffer.a3 " << a3->sstr() << "\n" ;
         a3->save("$FOLD/a3.npy");
         std::cout << "]NP_ReadToBufferCallback_test.CreateFromBuffer\n" ;
     }
-
-
 
     std::cout << "[NP_ReadToBufferCallback_test.serializeToBuffer\n" ;
     std::vector<char> buf1 ;
