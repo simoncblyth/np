23012485117ff5e4a871782b5f2cc87658d41d0f
Simon C Blyth <simoncblyth@gmail.com>
2025-11-17 16:00:48 +0800

    some int to size_t switches

--- tests/np_test.cc
+++ tests/np_test.cc
@@ -1,29 +1,29 @@
-//  ~/np/tests/np_test.sh 
+//  ~/np/tests/np_test.sh
 
 #include <cstdlib>
 #include "np.h"
 
 int main()
 {
-    std::vector<int> shape = {100,10,4,4} ; 
-    double* pp = new double[100*10*4*4] ; 
+    std::vector<int> shape = {100,10,4,4} ;
+    double* pp = new double[100*10*4*4] ;
 
-    int ni = shape[0] ; 
-    int nj = shape[1] ; 
-    int nk = shape[2] ; 
-    int nl = shape[3] ; 
+    int ni = shape[0] ;
+    int nj = shape[1] ;
+    int nk = shape[2] ;
+    int nl = shape[3] ;
 
-    for(int i=0 ; i < ni ; i++ ) 
-    for(int j=0 ; j < nj ; j++ ) 
-    for(int k=0 ; k < nk ; k++ ) 
-    for(int l=0 ; l < nl ; l++ ) 
+    for(int i=0 ; i < ni ; i++ )
+    for(int j=0 ; j < nj ; j++ )
+    for(int k=0 ; k < nk ; k++ )
+    for(int l=0 ; l < nl ; l++ )
     {
-        int idx = i*nj*nk*nl + j*nk*nl + k*nl + l ; 
-        double val = i*100000 + j*1000 + k*100 + l ; 
-        pp[idx] = val ; 
+        int idx = i*nj*nk*nl + j*nk*nl + k*nl + l ;
+        double val = i*100000 + j*1000 + k*100 + l ;
+        pp[idx] = val ;
     }
 
-    np::Write(getenv("NPY"), shape, pp, "<f8") ;   
+    np::Write(getenv("NPY"), shape, pp, "<f8") ;
 
-    return 0 ; 
-} 
+    return 0 ;
+}
