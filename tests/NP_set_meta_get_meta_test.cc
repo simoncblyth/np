25d9df33f40462e1b4285c97318f9815283785a3
Simon C Blyth <simoncblyth@gmail.com>
2026-04-24 15:56:35 +0800

    in NP::get_meta avoid need to manually set the fallback to empty string when T=std::string

--- tests/NP_set_meta_get_meta_test.cc
+++ tests/NP_set_meta_get_meta_test.cc
@@ -1,52 +1,139 @@
 // ~/np/tests/NP_set_meta_get_meta_test.sh
 
-const char* APATH = "$FOLD/a.npy" ; 
+const char* APATH = "$FOLD/a.npy" ;
 
 #include "NP.hh"
 
-
-void test_string()
+struct NP_set_meta_get_meta_test
 {
-    const char* o_msg = "metadata_string with some blank spaces on the line" ; 
+    static int uint64();
 
-    NP* a = NP::Make<float>(1,4,4); 
-    a->set_meta<std::string>("msg", o_msg) ; 
+    static int string();
+    static int string2();
+    static int string3();
+    static int string4();
 
-    std::string a_msg = a->get_meta<std::string>("msg", "" ); 
+    static int main();
+};
 
-    a->save(APATH); 
-    NP* b = NP::Load(APATH);  
+inline int NP_set_meta_get_meta_test::main()
+{
+    const char* TEST = U::GetEnv("TEST","string4");
+    bool ALL = strcmp(TEST,"ALL") == 0 ;
 
-    std::string b_msg = b->get_meta<std::string>("msg", "" ); 
-   
-    std::cout << "o_msg:[" << o_msg << "]" << std::endl ; 
-    std::cout << "a_msg:[" << a_msg << "]" << std::endl ; 
-    std::cout << "b_msg:[" << b_msg << "]" << std::endl ; 
+    int rc = 0 ;
+    if(ALL||0==strcmp(TEST,"uint64")) rc += uint64();
+    if(ALL||0==strcmp(TEST,"string")) rc += string();
+    if(ALL||0==strcmp(TEST,"string2")) rc += string2();
+    if(ALL||0==strcmp(TEST,"string3")) rc += string3();
+    if(ALL||0==strcmp(TEST,"string4")) rc += string4();
+
+    return rc ;
+}
+
+
+inline int NP_set_meta_get_meta_test::uint64()
+{
+    NP* a = NP::Make<float>(1,4,4);
+    uint64_t u0 = std::numeric_limits<uint64_t>::max() ;
+    a->set_meta<uint64_t>("u", u0 );
+    a->save(APATH);
+    NP* b = NP::Load(APATH);
+
+    uint64_t u1 = b->get_meta<uint64_t>("u", 0) ;
+
+    std::cout << " u0 " << u0 << std::endl ;
+    std::cout << " u1 " << u1 << std::endl ;
+    return 0 ;
+}
+
+
+
+inline int NP_set_meta_get_meta_test::string()
+{
+    const char* o_msg = "metadata_string with some blank spaces on the line" ;
+
+    NP* a = NP::Make<float>(1,4,4);
+    a->set_meta<std::string>("msg", o_msg) ;
+
+    std::string a_msg = a->get_meta<std::string>("msg", "" );
+
+    a->save(APATH);
+    NP* b = NP::Load(APATH);
+
+    std::string b_msg = b->get_meta<std::string>("msg", "" );
+
+    std::cout << "o_msg:[" << o_msg << "]" << std::endl ;
+    std::cout << "a_msg:[" << a_msg << "]" << std::endl ;
+    std::cout << "b_msg:[" << b_msg << "]" << std::endl ;
 
     /**
     epsilon:tests blyth$ cat /tmp/NP_set_meta_get_meta_test/a_meta.txt
     msg:metadata_string with some blank spaces on the line
     **/
+
+    return 0 ;
 }
 
-
-void test_uint64()
+inline int NP_set_meta_get_meta_test::string2()
 {
-    NP* a = NP::Make<float>(1,4,4); 
-    uint64_t u0 = std::numeric_limits<uint64_t>::max() ; 
-    a->set_meta<uint64_t>("u", u0 ); 
-    a->save(APATH);  
-    NP* b = NP::Load(APATH);  
- 
-    uint64_t u1 = b->get_meta<uint64_t>("u", 0) ; 
+    NP* a = NP::Make<float>(1,4,4);
+    a->set_meta<std::string>("red", "cyan") ;
+    a->set_meta<std::string>("green", "magenta") ;
+    a->set_meta<std::string>("blue", "yellow") ;
 
-    std::cout << " u0 " << u0 << std::endl ; 
-    std::cout << " u1 " << u1 << std::endl ; 
+    std::string s0 = a->get_meta<std::string>("red", "" );
+    std::string s1 = a->get_meta<std::string>("green", "" );
+    std::string s2 = a->get_meta<std::string>("blue", "" );
+    std::string s3 = a->get_meta<std::string>("missing", "" );
+
+    std::cout << " s0 [" << s0  << "]\n" ;
+    std::cout << " s1 [" << s1  << "]\n" ;
+    std::cout << " s2 [" << s2  << "]\n" ;
+    std::cout << " s3 [" << s3  << "]\n" ;
+
+    return 0 ;
 }
+
+inline int NP_set_meta_get_meta_test::string3()
+{
+    NP* a = NP::Make<float>(1,4,4);
+    a->meta = "QSim__simulate_tot_dt:0.0376182\nSettings:HitCompOneName:hit,PhotonCompOneName:photon\n" ;
+
+    std::string s0  = a->get_meta<std::string>("QSim__simulate_tot_dt", "" );
+    double d0       = a->get_meta<double>("QSim__simulate_tot_dt", 0.1 );
+
+    std::cout << " s0 [" << s0  << "]\n" ;
+    std::cout << " d0 [" << d0  << "]\n" ;
+    return 0 ;
+}
+
+inline int NP_set_meta_get_meta_test::string4()
+{
+    NP* a = NP::Make<float>(1,4,4);
+    a->meta = "QSim__simulate_tot_dt:0.0376182\nSettings:HitCompOneName:hit,PhotonCompOneName:photon\n" ;
+
+    std::string s0  = a->get_meta<std::string>("QSim__simulate_tot_dt", "" );
+    double d0       = a->get_meta<double>("QSim__simulate_tot_dt", 0.1 );
+
+    std::cout << " s0 [" << s0  << "]\n" ;
+    std::cout << " d0 [" << d0  << "]\n" ;
+
+
+    std::string s1  = a->get_meta<std::string>("Settings", "" );
+    std::cout << " s1 [" << s1  << "]\n" ;
+
+    std::string s2  = a->get_meta<std::string>("Settings");
+    std::cout << " s2 [" << s2  << "]\n" ;
+
+
+    return 0 ;
+}
+
+
+
 
 int main()
 {
-    test_uint64(); 
-
-    return 0 ; 
+    return NP_set_meta_get_meta_test::main();
 }
--- tests/NP_set_meta_get_meta_test.sh
+++ tests/NP_set_meta_get_meta_test.sh
@@ -8,11 +8,37 @@ EOU
 }
 cd $(dirname $(realpath $BASH_SOURCE))
 
-name=NP_set_meta_get_meta_test 
-export FOLD=/tmp/$name 
+name=NP_set_meta_get_meta_test
+export FOLD=/tmp/$name
 
 mkdir -p $FOLD
 bin=$FOLD/$name
 
-gcc $name.cc -std=c++11 -Wall -lstdc++ -I.. -DDEBUG -o $bin && $bin
+defarg=info_gcc_run
+arg=${1:-$defarg}
+
+
+vv="BASH_SOURCE PWD name FOLD bin defarg arg"
+
+if [[ "$arg" =~ info ]]; then
+    for v in $vv ; do printf "%30s : %s\n" "$v" "${!v}" ; done
+fi
+
+if [[ "$arg" =~ gcc ]]; then
+    gcc $name.cc -std=c++17 -Wall -lstdc++ -g -I.. -DDEBUG -o $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE - gcc fail && exit 1
+fi
+
+if [[ "$arg" =~ run ]]; then
+    $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE - run fail && exit 2
+fi
+
+if [[ "$arg" =~ dbg ]]; then
+    gdb -ex r --args $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE - run fail && exit 2
+fi
+
+exit 0
+
 
